144649ff2da7a7ced350a8d3091b13dec6102ab4
JusTMeth25 <lorenzo.melis@yahoo.it>
2026-06-30 16:13:06 +0200

    cambiati alcuni colori

--- src/assets/components/Jumbotron.jsx
+++ src/assets/components/Jumbotron.jsx
@@ -2,7 +2,7 @@
 function Jumbotron() {
   return (
     <>
-      <div className="card fw-bold text-center mb-5 p-3">
+      <div className="card cardJumbotron fw-bold text-center mb-4 p-3">
         <h1>Welcome to EpiBooks!</h1>
         <p className="fw-normal">We list books</p>
       </div>
